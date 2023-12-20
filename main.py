a7d4cf1b873d79d58c32a9910658536a7953766e
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-20 12:11:35 +0545

    Updated Bot

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ bot = telegram.Bot(token='6959113117:AAFPVCjeu_ZN4hwwIDHW09NpqNyEVCSnMG4')
 channel_id = '-1002109629328'
 
 
-# Getting the data from the database
-async def SendMessage(message):
-    try:
-        await bot.send_message(chat_id=channel_id, text=message)
-    except Exception as e:
-        print(f"Error sending message: {e}")
+# For Opening IPO
+async def OpeningIpo(name,totalunit):
+    message = f"New IPO of {name} is opening from Today with total unit {totalunit}. Please Don't miss the chance to apply"
+    # Send a text message
+    await bot.send_message(chat_id=channel_id, text=message)
+
+# Run the coroutine asynchronously
+# asyncio.run(send_message())
+
+
+# For Closing IPO
+async def ClosingIpo(name,totalunit):
+    message = f"Today is the Last Day to apply the IPO of {name} with total unit {totalunit}. Please Don't miss the chance to apply"
+    # Send a text message
+    await bot.send_message(chat_id=channel_id, text=message)
 
 
 # Connecting to the Database
@@ -20,28 +29,41 @@ try:
     connection = psycopg2.connect(connectionString)
     cursor = connection.cursor()
     print("Connected to PostgreSQL database successfully!")
-    SendMessage("Connected to PostgreSQL database successfully!")
 except Exception as e:
     print(f"Error connecting to database: {e}")
     exit(1)
 
 
+# for reading the data according to the opening date of the IPO
+# Reading the Data from the Database
+date = datetime.datetime.now().date()
+# date = '2019-12-12'
+# print(date)
+# query=f"select * from ipodetails where openingdate='{date}';"
+query=f"select * from ipodetails;"
+print(query)
+cursor.execute(query)
+result = cursor.fetchall()
+print(result)
+if result:
+    for row in result:
+        asyncio.run(OpeningIpo(row[0],row[1]))
+connection.commit()
 
-for i in range(1, 5):
-    try:
-        print("Sending message...")
-        asyncio.run(SendMessage(f'Hello World! {i}'))
-    except Exception as e:
-        print(f"Error sending message: {e}")
-
-for i in range(1,5):
-    try:
-        print("Sending message...")
-        asyncio.run(SendMessage(f'Hello Universe! {i}'))
-    except Exception as e:
-        print(f"Error sending message: {e}")
 
 
 
-# Writing the Data into Database System 
-# INSERT INTO ipodetails VALUES('Testing 4',10000,'2000-01-01','2001-02-02');
+# for Reading the data according to the closing date fo the ipo 
+# Reading the Data from the Database
+date = datetime.datetime.now().date()
+print(date)
+# query=f"select * from ipodetails where closingdate='{date}';"
+query=f"select * from ipodetails;"
+print(query)
+cursor.execute(query)
+result = cursor.fetchall()
+print(result)
+if result:
+    for row in result:
+        asyncio.run(ClosingIpo(row[0],row[1]))
+connection.commit()
