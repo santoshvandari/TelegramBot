5c601c004c48969cdb579880756906fcf7ec8999
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-21 19:01:59 +0545

    Initial

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import psycopg2
-import datetime
-import asyncio
-import telegram
+import psycopg2,datetime,asyncio,telegram
 
 bot = telegram.Bot(token='6959113117:AAFPVCjeu_ZN4hwwIDHW09NpqNyEVCSnMG4')
 channel_id = '-1002109629328'
@@ -34,40 +31,24 @@ async def ClosingIpo(companyname,symbol, totalunit,issuetype,issuemanager,openin
 
 # Reading the Data from the Database for opening date
 date = datetime.datetime.now().date()
-query = f"select * from ipoinfodetails;"
-print(query)
+query = f"select * from ipoinfodetails where openingdate='{date}';"
 cursor.execute(query)
 result = cursor.fetchall()
-# print(result)
-
 loop = asyncio.get_event_loop()
-
 if result:
     print("For Opening IPO")
     for row in result:
-        # print(f'{row[0]}\t {row[1]}\t {row[2]}\t {row[3]}\t {row[4]}\t {row[5]}\t {row[6]}\n')
-        # print(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
         loop.run_until_complete(OpeningIpo(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
         print("opening Message Sent")
-        # break
-
 
 # # Reading the Data from the Database for closing date
-# date = datetime.datetime.now().date()
-# print(date)
-# query = f"select * from ipoinfodetails;"
-
-# cursor.execute(query)
-# result = cursor.fetchall()
-# # print(result)
-
+query = f"select * from ipoinfodetails where closingdate='{date}';"
+cursor.execute(query)
+result = cursor.fetchall()
 if result:
-    print("For Opening IPO")
+    print("For Closing IPO")
     for row in result:
-        # print(f'{row[0]}\t {row[1]}\t {row[2]}\t {row[3]}\t {row[4]}\t {row[5]}\t {row[6]}\n')
-        # print(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
         loop.run_until_complete(ClosingIpo(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
         print("Closing Message Sent")
-        # break
 
-# connection.commit()
+connection.commit()
